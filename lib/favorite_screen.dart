e85ee69070b39ac051a2534cc974bbb438d4137c
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-05-21 08:57:05 +0700

    step 15. show favorite screen (completed)

--- lib/favorite_screen.dart
+++ lib/favorite_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'meal.dart';
+import 'recipe_item.dart';
 
 class FavoriteScreen extends StatelessWidget {
   final List<Meal> favoriteMeals;
@@ -7,8 +8,24 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Favorite Screen'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text("No favorites yet - start adding some!"),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return RecipeItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            complexity: favoriteMeals[index].complexity,
+            affordability: favoriteMeals[index].affordability,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
